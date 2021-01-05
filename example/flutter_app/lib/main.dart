ec7cdc88b23d287121721fc6ccccff04bc1e92d8
Jasm Sison <jasm.sison@gmail.com>
2021-01-05 22:36:55 +0100

    android example done, only the audio channel is shut down before it has a chance to play

--- example/flutter_app/lib/main.dart
+++ example/flutter_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:ffi';
 import 'dart:io';
 import 'package:ffi/ffi.dart';
@@ -5,6 +6,8 @@ import 'package:ffi/ffi.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:miniaudio_dart/miniaudio.dart' as ma;
+
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -34,38 +37,49 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   ma.MiniAudioC bindings;
   int _result = 0;
-  List<FileSystemEntity> paths;
-  String backPath;
+  List<String> assets;
+  Directory dir;
 
   @override
   void initState() {
     super.initState();
-    getApplicationDocumentsDirectory().then((dir) async {
-      paths = await dir.list(recursive: true).toList();
-      backPath = FileSystemEntity.parentOf(dir.path);
+    getApplicationDocumentsDirectory().then((d) async {
+      final assetManifestJsonString =
+          await rootBundle.loadString('AssetManifest.json');
+
+      final decoded = jsonDecode(assetManifestJsonString);
+      assets = decoded.values.fold([], (x, y) => x + y).cast<String>();
+
+      dir = d;
+
       bindings = ma.bindings;
       setState(() {});
     });
   }
 
   Future<void> _player(String path) async {
-    final fileType = FileSystemEntity.typeSync(path);
-    if (fileType == FileSystemEntityType.file) {
-      final filenames = ['dummy', path];
-      Pointer<Pointer<Int8>> argv = allocate(count: 2);
-      final utf8s = filenames.map(Utf8.toUtf8).toList();
-      [0, 1].forEach((i) => argv[i] = utf8s[i].cast<Int8>());
-      final result = bindings.single_playback(2, argv);
-      free(argv);
+    String cachePath = '${dir.path}/$path';
 
-      setState(() {
-        _result = result;
+    // on demand copy
+    if (!FileSystemEntity.isFileSync(cachePath)) {
+      ByteData data = await rootBundle.load(path);
+      List<int> bytes =
+          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      await File(cachePath).create(recursive: true).then((file) {
+        file.writeAsBytes(bytes);
       });
-    } else if (fileType == FileSystemEntityType.directory) {
-      paths = await Directory(path).list(recursive: true).toList();
-      backPath = FileSystemEntity.parentOf(path);
-      setState(() {});
     }
+
+    final filenames = ['dummy', cachePath];
+    Pointer<Pointer<Int8>> argv = allocate(count: 2);
+    final utf8s = filenames.map(Utf8.toUtf8).toList();
+    [0, 1].forEach((i) => argv[i] = utf8s[i].cast<Int8>());
+    final result = bindings.single_playback(2, argv);
+    free(argv);
+
+    setState(() {
+      _result = result;
+    });
   }
 
   @override
@@ -73,20 +87,14 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('result: $_result'),
-        leading: IconButton(
-            icon: Icon(Icons.arrow_back), onPressed: () => _player(backPath)),
       ),
       body: ListView.builder(
         itemBuilder: (context, index) => ListTile(
             title: Text(
-              paths[index].path,
-              style: FileSystemEntity.typeSync(paths[index].path) ==
-                      FileSystemEntityType.directory
-                  ? TextStyle(fontWeight: FontWeight.bold)
-                  : TextStyle(color: Colors.grey),
+              assets[index],
             ),
-            onTap: () => _player(paths[index].path)),
-        itemCount: paths != null ? paths.length : 0,
+            onTap: () => _player(assets[index])),
+        itemCount: assets != null ? assets.length : 0,
       ),
     );
   }
