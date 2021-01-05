b75b4f76e2df46155233493c633f0b67926b8741
Jasm Sison <jasm.sison@gmail.com>
2021-01-05 04:11:58 +0100

    added wav test, couldn't hear it tho

--- test/miniaudio-dart_test.dart
+++ test/miniaudio-dart_test.dart
@@ -1,16 +1,31 @@
-import 'package:miniaudio-dart/miniaudio-dart.dart';
+import 'dart:ffi';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:convert';
+import 'package:ffi/ffi.dart';
+
+import '../lib/src/bindings/bindings.dart';
+import '../lib/src/bindings/miniaudio-c.dart';
 import 'package:test/test.dart';
 
+MiniAudioC b;
+
 void main() {
-  group('A group of tests', () {
-    Awesome awesome;
+  group('A playback test group', () {
+
 
     setUp(() {
-      awesome = Awesome();
+      b = bindings;
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('play wav', () {
+      final filenames = ['dummy', 'media/taunt.wav'];
+      Pointer<Pointer<Int8>> argv = allocate(count: 2);
+      final utf8s = filenames.map(Utf8.toUtf8).toList();
+      [0,1].forEach((i) => argv[i] = utf8s[i].cast<Int8>());
+      final result = b.single_playback(2, argv);
+      expect(result, 0);
+      free(argv);
     });
   });
 }
